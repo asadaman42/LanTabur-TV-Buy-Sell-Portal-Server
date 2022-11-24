6bb743bbbe671683c70cd0cb56e46a4e8c71cbaf
asadaman42 <asadaman42@gmail.com>
2022-11-24 16:57:41 +0600

    server created

---
+++ index.js
@@ -0,0 +1,39 @@
+// required
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+// const jwt = require('jsonwebtoken');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.LANTABUR_USER}:${process.env.LANTABUR_PASSWORD}@asadaman42.mzbtlu2.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try{
+
+    }
+
+    finally{
+
+    }
+}
+
+run().catch(er => console.error(er));
+
+/*************** 
+    Operation
+***************/
+
+app.get('/', (req, res) => {
+    res.send('Server is okay.')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port - ${port}`)
+})

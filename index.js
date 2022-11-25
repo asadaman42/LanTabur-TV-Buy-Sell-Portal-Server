b192e22b0bc4fd060b6a86c369347c7af620a4c7
asadaman42 <asadaman42@gmail.com>
2022-11-25 07:23:38 +0600

    product shown

--- index.js
+++ index.js
@@ -10,29 +10,34 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.LANTABUR_USER}:${process.env.LANTABUR_PASSWORD}@asadaman42.mzbtlu2.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
-    try{
-        const serviceCollection = client.db('LanTaburTv').collection('categories');
+    try {
+        const categoriesCollection = client.db('LanTaburTv').collection('categories');
         
-        app.get('/services', async (req, res) => {
+
+        app.get('/categories', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-
-
-
+            const cursor = categoriesCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        });
+        app.get('/category/:categoryId', async (req, res) => {
+            const id = req.params.categoryId;
+            const query = { _id: ObjectId(id) };
+            const category = await categoriesCollection.findOne(query);
+            console.log(category);
+            res.send(category);
+        });
     }
 
-    finally{
+    finally {
 
     }
 }
-
 run().catch(er => console.error(er));
 
 /*************** 

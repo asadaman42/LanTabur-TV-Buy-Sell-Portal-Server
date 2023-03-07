c859bb704884f3def8852f691635f0ac12bb9f52
asadaman42 <asadaman42@gmail.com>
2023-03-07 16:06:29 +0600

    project changed in mongodb

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const e = require('express');
 require('dotenv').config();
 // const jwt = require('jsonwebtoken');
 const app = express();
@@ -10,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.LANTABUR_USER}:${process.env.LANTABUR_PASSWORD}@asadaman42.mzbtlu2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.LANTABUR_USER}:${process.env.LANTABUR_PASSWORD}@lantaburtvbuyandsellpor.1h9iyo0.mongodb.net/?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -22,81 +21,49 @@ const run = async () => {
         const categoriesCollection = client.db('LanTaburTv').collection('categories');
         const bookingCollection = client.db('LanTaburTv').collection('bookings');
         const usersCollection = client.db('LanTaburTv').collection('users');
+        const advertiseCollection = client.db('LanTaburTv').collection('advertisement');
 
 
 
-        // for loading all categories.
-        app.get('/categories', async (req, res) => {
-            const query = {};
-            const cursor = categoriesCollection.find(query);
-            const categories = await cursor.toArray();
-            res.send(categories);
-        });
+        // for my product of seller    
+        app.get('/products/:email', async (req, res) => {
+            const email = req.params.email;
+            // const result = await categoriesCollection
+            //     .find({ products: { $elemMatch: { sellerEmail: email } } })
+            //     .project({ products: 1, _id: 0 })
+            //     .toArray();
 
-        // to load all products under specified category.
-        app.get('/category/:categoryId', async (req, res) => {
-            const id = req.params.categoryId;
-            const query = { _id: ObjectId(id) };
-            const category = await categoriesCollection.findOne(query);
-            res.send(category);
-        });
-
-        // for add products
-        app.post('/category/:categoryName', async (req, res) => {
-            const name = req.params.categoryName;
-            const newProduct = req.body;
-            newProduct._id = ObjectId();
-            const query = { categoryName: name }
-            const result = await categoriesCollection.updateOne(query, { $push: { "products": newProduct } });
-            res.send(result);
-        })
-
-        // for creating booking database
-        app.post('/bookings', async (req, res) => {
-            const booking = req.body;
-            const result = await bookingCollection.insertOne(booking);
-            res.send(result);
-        })
-
-        // for getting My products for seller
-        app.get('/bookings', async (req, res) => {
-            const email = req.query.email;
-            const query = { buyerEmail: email };
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
-        })
-
-        // for posting user information to db
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
-        })
-
-
-        // for getting All Sellers or Buyers
-        app.get('/users', async (req, res) => {
-            const userType = req.query.userType;
-            const query = { buyerOrSeller: userType };
-            const allSellers = await usersCollection.find(query).toArray();
-            res.send(allSellers);
-        })
-
-        // for verifiying the seller
-        app.put('/user/verification/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const options = { upser: true };
-            const updatedDoc = {
-                $set: {
-                    isVerified: true
+            const result = await categoriesCollection.aggregate([
+                {
+                    $match: {
+                        "products.sellerEmail": email
+                    }
+                },
+                {
+                    $project: {
+                        "products": {
+                            $filter: {
+                                input: "$products",
+                                cond: {
+                                    $eq: [
+                                        "$$this.sellerEmail",
+                                        email
+                                    ]
+                                }
+                            }
+                        }
+                    }
                 }
-            }
-            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            ]).toArray()
+
+            console.log(result);
+
+
             res.send(result);
         })
 
-        // delete user 
+
+        // delete user => OK
         app.delete('/user/delete/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -106,8 +73,151 @@ const run = async () => {
 
 
 
+        // delete product => OK 
+        app.post('/myproducts/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const company = req.body.company;
+            const filter = { categoryName: company };
+            const options = { multi: true };
+            const updatedDoc = {
+                $pull: { products: { _id: ObjectId(id) } }
+            }
+            const result = await categoriesCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
 
-        // checkig if the user is admin 
+
+
+        // for getting My orders for Buyer
+        //  Used in MyOrders.js
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { buyerEmail: email };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+
+
+        // for creating booking database => OK
+        // Used in category.js  AdvertisedITems.js
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+
+
+        // for showing advertied ITems in Homepage => OK
+        app.get('/advertise', async (req, res) => {
+            const result = await advertiseCollection.find({}).toArray();
+            res.send(result);
+        })
+
+
+
+        // for adding products into advertisement db => OK
+        app.post('/advertise', async (req, res) => {
+            const advertisementData = req.body;
+            const filter = { oldID: advertisementData.oldID };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: advertisementData
+            }
+            const result = await advertiseCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+
+
+        // for loading all categories. => OK
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const cursor = categoriesCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        });
+
+
+
+        // to load all products under specified category. => OK
+        app.get('/category/:categoryId', async (req, res) => {
+            const id = req.params.categoryId;
+            const query = { _id: ObjectId(id) };
+            const category = await categoriesCollection.findOne(query);
+            res.send(category);
+        });
+
+
+
+        // Adding products for seller => OK
+        app.post('/category/:categoryName', async (req, res) => {
+            const name = req.params.categoryName;
+            const newProduct = req.body;
+            newProduct._id = ObjectId();
+            const query = { categoryName: name }
+            const result = await categoriesCollection.updateOne(query, { $push: { "products": newProduct } });
+            res.send(result);
+        })
+
+
+
+        // for posting user information to db => OK
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+
+
+        // for getting All Sellers or Buyers => OK
+        app.get('/users', async (req, res) => {
+            const userType = req.query.userType;
+            const query = { buyerOrSeller: userType };
+            const allSellers = await usersCollection.find(query).toArray();
+            res.send(allSellers);
+        })
+
+
+
+        // for getting specific Sellers or Buyers => OK
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send(user);
+        })
+
+
+
+        // for verifiying the seller => OK
+        app.put('/user/verification/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    isVerified: true
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+
+
+        
+
+
+
+        // checkig if the user is admin  => OK
         app.get('/user/admin/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email };
@@ -115,9 +225,29 @@ const run = async () => {
             res.send({ isAdmin: user?.role === 'admin' });
         })
 
-        
 
-        // Making a User admin 
+
+        // checkig if the user is Seller  => OK
+        app.get('/user/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isSeller: user?.buyerOrSeller === 'Seller' });
+        })
+
+
+
+        // checkig if the user is Buyer  => OK
+        app.get('/user/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isBuyer: user?.buyerOrSeller === 'Buyer' });
+        })
+
+
+
+        // Making a User admin  => OK
         app.put('/user/admin/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -129,11 +259,7 @@ const run = async () => {
             }
             const result = await usersCollection.updateOne(query, updatedDoc, options);
             res.send(result);
-        })
-
-
-
-
+        });
     }
     finally { }
 }
